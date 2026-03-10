22135596f00a33b729c1a0a550b9fc76225f59ac
Kahivandekell <chrostowskialekss@gmail.com>
2026-03-10 02:05:45 +0100

    dodanie sortowania za pomoca wartosci min i max

--- src/components/SortSelect/SortSelect.js
+++ src/components/SortSelect/SortSelect.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './SortSelect.css'
-import { useState, createContext} from "react";
 import products from "../../data/products";
 
 
 
 
-export default function SortSelect({ searchTerm, setSearchTerm }) {
+export default function SortSelect({ 
+    searchTerm, setSearchTerm,
+    minPrice, setMinPrice,
+    maxPrice, setMaxPrice,
+}) {
 
     const minProductValue = Math.min(...products.map(val => (val.price)))
     const maxProductValue = Math.max(...products.map(val => (val.price)))
@@ -15,8 +18,8 @@ export default function SortSelect({ searchTerm, setSearchTerm }) {
         <div className="search-container">
             <form className="search-form" role="search">
                 <input className="search-input" type="search" placeholder="Search" value={searchTerm} onChange={(val) => setSearchTerm(val.target.value)}/>
-                <input className="search-input" type="number" placeholder={minProductValue}/>
-                <input className="search-input" type="number" placeholder={maxProductValue}/>
+                <input className="search-input" type="number" placeholder="min Value" value={minPrice} onChange={(val) => setMinPrice(Number(val.target.value))}/>
+                <input className="search-input" type="number" placeholder="max Value" value={maxPrice} onChange={(val) => setMaxPrice(Number(val.target.value))}/>
                 
                 <button className="btn btn-dark" type="button">up</button>
             </form>
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -14,16 +14,32 @@ export default function MainPage (){
     const [maxPrice, setMaxPrice] = useState("")
     const [sortPrice, setSortPrice] = useState("")
 
-    const sortProducts = products.filter((product) => (
-        product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    ))
+    const sortProductsByName = products.filter((product) => {
+        const matchesName = product.name.toLowerCase().includes(searchTerm.toLowerCase()) || searchTerm === ""
+        const minimum = product.price >= minPrice || minPrice === ""
+        const maximum = product.price <= maxPrice || maxPrice === ""
+        const sortPrice = product.price 
+        return (matchesName && minimum && maximum)
+    
+    })
+
+
+
+/* 
+    const sortPrice = products.filter((product) => (
+        product.price
+    )) */
 
     return(
         <div className="container text-center">
-            <SortSelect searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+            <SortSelect 
+            searchTerm={searchTerm} setSearchTerm={setSearchTerm}
+            minPrice={minPrice} setMinPrice={setMinPrice}
+            maxPrice={maxPrice} setMaxPrice={setMaxPrice}
+            />
 
             <div className="row gx-4 gy-4">
-                {sortProducts.map((products) => (
+                {sortProductsByName.map((products) => (
                 <div key={products.id} className='col-12 col-sm-6 col-md-4'> 
                     <div className='single-product'>
                         <h2>{products.name}</h2>
